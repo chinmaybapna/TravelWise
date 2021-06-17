429a6b9c71fe0c77d18ac522c90fcb88ac64ba41
Chinmay Bapna <chinmaybapna111.cb@gmail.com>
2021-06-17 16:13:06 +0530

    added textview to add hashtags

--- TravelWise/Views/EndTripViewController.swift
+++ TravelWise/Views/EndTripViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 import Firebase
 import RangeSeekSlider
 
-class EndTripViewController: UIViewController {
+class EndTripViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var rangeSlider: RangeSeekSlider!
     @IBOutlet weak var shareTrip: UISwitch!
     @IBOutlet weak var tripNameLabel: UILabel!
     @IBOutlet weak var tripProfileImageView: UIImageView!
+    @IBOutlet weak var hashtagTextView: UITextView!
     
     var currentTripID: String?
     var uid = UserDefaults.standard.string(forKey: "uid")
@@ -31,6 +32,17 @@ class EndTripViewController: UIViewController {
         self.tripProfileImageView.sd_setImage(with: URL(string: self.tripProfileImageURL!), placeholderImage: UIImage(named: "rowan-heuvel-U6t80TWJ1DM-unsplash"))
         rangeSlider.minLabelFont = UIFont.systemFont(ofSize: 15)
         rangeSlider.maxLabelFont = UIFont.systemFont(ofSize: 15)
+        
+        hashtagTextView.delegate = self
+        
+        hashtagTextView.text = "Add hashtags"
+        hashtagTextView.textColor = UIColor.lightGray
+        
+        hashtagTextView.layer.borderWidth = 0.5
+        hashtagTextView.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        hashtagTextView.layer.cornerRadius = 5
+        
+        hashtagTextView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
         
     }
     
