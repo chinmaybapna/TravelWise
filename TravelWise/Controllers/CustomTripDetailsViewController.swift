fc61befe88cfd5f259122eb5f04c7ec43548168e
nidhi <nidhibhatg17@gmail.com>
2022-06-02 21:14:10 +0530

    Plan itinerary UI improvements and fixes

--- TravelWise/Controllers/CustomTripDetailsViewController.swift
+++ TravelWise/Controllers/CustomTripDetailsViewController.swift
@@ -6,15 +6,100 @@
 //
 
 import UIKit
+import RangeSeekSlider
 
-class CustomTripDetailsViewController: UIViewController {
+class CustomTripDetailsViewController: UIViewController, RangeSeekSliderDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var startDateTextField: UITextField! {
+        didSet {
+            let blackPlaceholderText = NSAttributedString(string: "DD/MM/YYYY",
+                                                          attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+            
+            startDateTextField.attributedPlaceholder = blackPlaceholderText
+        }
+    }
+    @IBOutlet weak var endDateTextField: UITextField! {
+        didSet {
+            let blackPlaceholderText = NSAttributedString(string: "DD/MM/YYYY",
+                                                          attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+            
+            endDateTextField.attributedPlaceholder = blackPlaceholderText
+        }
+    }
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var rangeSlider: RangeSeekSlider!
+    @IBOutlet weak var rangeLabel: UILabel! {
+        didSet {
+            rangeLabel.text = "₹ 5000 - ₹ 50000"
+        }
     }
     
+    private var datePicker : UIDatePicker?
+    private var datePicker1 : UIDatePicker?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rangeSlider.minLabelFont = UIFont.systemFont(ofSize: 15)
+        rangeSlider.maxLabelFont = UIFont.systemFont(ofSize: 15)
+
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        startDateTextField.inputView = datePicker
+        datePicker?.addTarget(self, action: #selector(startDateChanged(datePicker:)), for: .valueChanged)
+        
+        datePicker1 = UIDatePicker()
+        datePicker1?.datePickerMode = .date
+        endDateTextField.inputView = datePicker1
+        datePicker1?.addTarget(self, action: #selector(endDateChanged(datePicker1:)), for: .valueChanged)
+        
+        startDateTextField.layer.borderWidth = 0.5
+        startDateTextField.layer.cornerRadius = 5
+        startDateTextField.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        startDateTextField.setLeftPaddingPoints(10)
+        startDateTextField.setRightPaddingPoints(10)
+        
+        endDateTextField.layer.borderWidth = 0.5
+        endDateTextField.layer.cornerRadius = 5
+        endDateTextField.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        endDateTextField.setLeftPaddingPoints(10)
+        endDateTextField.setRightPaddingPoints(10)
+        
+        nextButton.layer.cornerRadius = 5
+        
+        rangeSlider.delegate = self
+        
+//        rangeSlider.addTarget(self, action: #selector(rangeChanged(rangeSlider:)), for: UIControl.Event)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        view.addGestureRecognizer(tapGesture)
+        // Do any additional setup after loading the view.
+    }
+    func rangeSeekSlider(_ slider: RangeSeekSlider, didChange minValue: CGFloat, maxValue: CGFloat)
+    {
+        rangeLabel.text = "₹ \(rangeSlider.selectedMinValue)- ₹ \(rangeSlider.selectedMaxValue)"
+    }
+    @objc func viewTapped() {
+        print("dfbjshbcjsb")
+        view.endEditing(true)
+    }
+    
+    @objc func startDateChanged(datePicker : UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        startDateTextField.text = dateFormatter.string(from: datePicker.date)
+    }
+    
+//    @objc func rangeChanged(rangeSlider: RangeSeekSlider) {
+//        rangeLabel.text = "₹ \(rangeSlider.selectedMinValue)- ₹ \(rangeSlider.selectedMaxValue)"
+//    }
+    
+    @objc func endDateChanged(datePicker1 : UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        endDateTextField.text = dateFormatter.string(from: datePicker1.date)
+    }
 
     /*
     // MARK: - Navigation
--- TravelWise/Controllers/SelectCustomCityViewController.swift
+++ TravelWise/Controllers/SelectCustomCityViewController.swift
@@ -9,13 +9,36 @@ import UIKit
 
 class SelectCustomCityViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var cityNameTextField: UITextField! {
+        didSet {
+            let blackPlaceholderText = NSAttributedString(string: "Enter city name",
+                                                          attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+            
+            cityNameTextField.attributedPlaceholder = blackPlaceholderText
+        }
     }
     
+    @IBOutlet weak var nextButton: UIButton!
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+         cityNameTextField.layer.borderWidth = 0.5
+         cityNameTextField.layer.cornerRadius = 5
+         cityNameTextField.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+         cityNameTextField.setLeftPaddingPoints(10)
+         cityNameTextField.setRightPaddingPoints(10)
+         
+         nextButton.layer.cornerRadius = 5
+         
+         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+         view.addGestureRecognizer(tapGesture)
 
+         // Do any additional setup after loading the view.
+     }
+    @objc func viewTapped() {
+        view.endEditing(true)
+    }
     /*
     // MARK: - Navigation
 
